0411a1baae92050bb2c76f47aff3d313d29d771c
iskcal <cross_icon@outlook.com>
2021-05-26 22:08:49 +0800

    define a client information

--- IdentityServerHost/Config.cs
+++ IdentityServerHost/Config.cs
@@ -25,33 +25,29 @@ namespace IdentityServerHost
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-                // m2m client credentials flow client
                 new Client
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-                    
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
+                    ClientId = "ReactClient",
+                    ClientName="ReactClient",
+                    ClientUri = "https://localhost:3000",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    RequireClientSecret = false,
+                    RedirectUris = 
+                    {
+                        "https://localhost:3000/signin-oidc",
+                    },
+                    PostLogoutRedirectUris = 
+                    {
+                        "https://localhost:3000/signout-oidc",
+                    },
+                    AllowedScopes = 
+                    {
+                        "openid",
+                        "profile",
+                        "api",
+                    },
+                    AllowAccessTokensViaBrowser = true,
+                }
             };
     }
 }
